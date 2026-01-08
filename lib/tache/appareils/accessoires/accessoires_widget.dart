16674439cbce61b8aa5556babe28edcf2b5af751
detroithitechdev3 <detroithitech.dev3@gmail.com>
2026-01-08 11:07:29 +0100

    fix:responsive page accessoires

--- lib/tache/appareils/accessoires/accessoires_widget.dart
+++ lib/tache/appareils/accessoires/accessoires_widget.dart
@@ -87,10 +87,13 @@ class _AccessoiresWidgetState extends State<AccessoiresWidget> {
               Align(
                 alignment: AlignmentDirectional(0.0, 0.0),
                 child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                  padding: EdgeInsetsDirectional.fromSTEB(8.0, 10.0, 8.0, 0.0),
                   child: Container(
-                    width: 377.0,
-                    height: 55.12,
+                    width: double.infinity,
+                    constraints: const BoxConstraints(
+                      minHeight: 50,
+                      maxHeight: 60,
+                    ),
                     decoration: BoxDecoration(
                       color: Color(0xFF1CC88A),
                       boxShadow: [
@@ -152,9 +155,9 @@ class _AccessoiresWidgetState extends State<AccessoiresWidget> {
                 ),
               ),
               Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(5.0, 10.0, 0.0, 0.0),
+                padding: EdgeInsetsDirectional.fromSTEB(8.0, 10.0, 8.0, 0.0),
                 child: Container(
-                  width: 377.0,
+                  width: double.infinity,
                   child: TextFormField(
                     controller: _model.textController,
                     focusNode: _model.textFieldFocusNode,
