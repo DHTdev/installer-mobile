3d6961a91b710294a70eec498263a66f9accce82
detroithitechdev3 <detroithitech.dev3@gmail.com>
2026-01-08 11:01:08 +0100

    fix:responsive page SIM

--- lib/tache/appareils/sim/sim_widget.dart
+++ lib/tache/appareils/sim/sim_widget.dart
@@ -89,10 +89,13 @@ class _SimWidgetState extends State<SimWidget> {
               Align(
                 alignment: AlignmentDirectional(0.0, 0.0),
                 child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                  padding: EdgeInsetsDirectional.fromSTEB(8.0, 10.0, 8.0, 0.0),
                   child: Container(
-                    width: 377.0,
-                    height: 55.12,
+                    width: double.infinity,
+                    constraints: const BoxConstraints(
+                      minHeight: 50,
+                      maxHeight: 60,
+                    ),
                     decoration: BoxDecoration(
                       color: Color(0xFF1CC88A),
                       boxShadow: [
@@ -154,9 +157,9 @@ class _SimWidgetState extends State<SimWidget> {
                 ),
               ),
               Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(5.0, 10.0, 0.0, 0.0),
+                padding: EdgeInsetsDirectional.fromSTEB(8.0, 10.0, 8.0, 0.0),
                 child: Container(
-                  width: 377.0,
+                  width: double.infinity,
                   child: TextFormField(
                     controller: _model.textController,
                     focusNode: _model.textFieldFocusNode,
