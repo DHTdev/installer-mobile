0167434d4f8598dcb95dc281ef6d99f03ecac7e8
detroithitechdev3 <detroithitech.dev3@gmail.com>
2026-01-09 11:18:07 +0100

    fix: show FCM notifications in foreground with flutter_local_notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:mobile_installer/firebase_options.dart';
+import 'package:mobile_installer/notification_service.dart';
 import 'package:mobile_installer/notifications/notifications_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
@@ -12,16 +13,13 @@ import 'flutter_flow/flutter_flow_util.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'index.dart';
 
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-// Handle background message (log, analytics, local DB, etc.)
-  debugPrint('BG message: \\${message.messageId} data=\\${message.data}');
-}
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+    NotificationService.initializeNotification();
+    FirebaseMessaging.onBackgroundMessage(NotificationService.firebaseMessagingBackgroundHandler);
   GoRouter.optionURLReflectsImperativeAPIs = true;
   usePathUrlStrategy();
 
@@ -68,7 +66,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    firebaseMessaging();
+    // firebaseMessaging();
 
     _appStateNotifier = AppStateNotifier.instance;
     _router = createRouter(_appStateNotifier);
@@ -79,56 +77,6 @@ class _MyAppState extends State<MyApp> {
         FlutterFlowTheme.saveThemeMode(ThemeMode.light);
       });
 
-void firebaseMessaging() async {
-  final _firebaseMessaging = FirebaseMessaging.instance;
-  final fcmToken = await _firebaseMessaging.getToken();
-  print('FCM Token: ${fcmToken}');
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('message: ${message.notification?.title}');
-    final title = message.notification?.title ?? "N/A";
-    final body = message.notification?.body ?? "N/A";
-
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: Text(
-          body,
-          maxLines: 1,
-          style: TextStyle(overflow: TextOverflow.ellipsis),
-        ),
-      ),
-    );
-  });
-
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-    print('message: ${message.notification?.title}');
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => NotificationsWidget()),
-    );
-  });
-
-  FirebaseMessaging.instance.getInitialMessage().then((message) {
-    if(message!=null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => NotificationsWidget()),
-      );
-    }
-  });
-}
-
-Future<void> checkInitialMessage() async {
-  final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-  if (initialMessage != null) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => NotificationsWidget()),
-    );
-  }
-}
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/notification_service.dart
@@ -0,0 +1,118 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'firebase_options.dart';
+
+class NotificationService {
+
+  static final  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+
+  @pragma('vm:entry-point')
+  static Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+// Handle background message (log, analytics, local DB, etc.)
+    debugPrint('BG message: \\${message.messageId} data=\\${message.data}');
+
+    await _initializeLocalNotification();
+    await _showFlutterNotification(message);
+
+  }
+
+  static Future<void> _showFlutterNotification(RemoteMessage message) async {
+    RemoteNotification? notification = message.notification;
+    Map<String, dynamic>? data = message.data;
+
+    final title = message.notification?.title ?? "N/A";
+    final body = message.notification?.body ?? "N/A";
+
+    AndroidNotificationDetails androidNotificationDetails =
+    AndroidNotificationDetails(
+      'mobile_installer_notifications_default',
+      'Notifications importantes',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      playSound: true,
+      enableVibration: true,
+      visibility: NotificationVisibility.public,
+    );
+
+    DarwinNotificationDetails iOSDetails=const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: iOSDetails
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+        DateTime.now().millisecondsSinceEpoch ~/ 1000,
+        title,
+        body,
+        notificationDetails
+    );
+  }
+
+  static Future<void> _initializeLocalNotification() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    final DarwinInitializationSettings initializationSettingsIOS =
+    DarwinInitializationSettings();
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response){
+      print (response.payload);
+      }
+    );
+  }
+
+  static Future<void> initializeNotification() async {
+    final _firebaseMessaging = FirebaseMessaging.instance;
+    final fcmToken = await _firebaseMessaging.getToken();
+    print('FCM Token: ${fcmToken}');
+
+    await _firebaseMessaging.requestPermission();
+
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      _showFlutterNotification(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('app open from background notification: ${message.notification?.title}');
+    });
+
+    await _getFcmToken();
+    await _initializeLocalNotification();
+    await _getInitialNotification();
+
+  }
+
+  static Future<void> _getFcmToken() async {
+    final fcmToken = await _firebaseMessaging.getToken();
+    print('FCM Token: ${fcmToken}');
+  }
+
+  static Future<void> _getInitialNotification() async {
+    RemoteMessage ? message = await FirebaseMessaging.instance.getInitialMessage();
+      
+    if(message!=null){
+      print('App launched from terminated');
+    }
+  }
+
+}
